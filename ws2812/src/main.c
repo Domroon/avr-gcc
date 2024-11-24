df53c07812e734d191ddeccbbcba1282aeeabae8
Domroon <haeusser-dominik@web.de>
2024-11-24 07:31:04 +0100

    transform to ws2812 blink

--- ws2812/src/main.c
+++ ws2812/src/main.c
@@ -15,19 +15,22 @@
 struct cRGB colors[8];
 struct cRGB led[MAXPIX];
 
-int main(void)
-{
-	
-	uint8_t j = 1;
-	uint8_t k = 1;
 
-	DDRB|=_BV(ws2812_pin);
-		
+void clear_leds(){
     uint8_t i;
     for(i=MAXPIX; i>0; i--)
     {    
         led[i-1].r=0;led[i-1].g=0;led[i-1].b=0;
     }
+}
+
+
+int main(void)
+{
+	DDRB|=_BV(ws2812_pin);
+		
+    clear_leds();
+    ws2812_setleds(led,MAXPIX);
 		
     //Rainbowcolors
     colors[0].r=150; colors[0].g=150; colors[0].b=150;
@@ -38,47 +41,15 @@ int main(void)
     colors[5].r=000; colors[5].g=100; colors[5].b=255;//light blue (türkis)
     colors[6].r=000; colors[6].g=000; colors[6].b=255;//blue
     colors[7].r=100; colors[7].g=000; colors[7].b=255;//violet
+    colors[8].r=000; colors[8].g=000; colors[8].b=000;//off
     
 	while(1)
     {
-        //shift all vallues by one led
-        uint8_t i=0;           
-        for(i=MAXPIX; i>1; i--) 
-            led[i-1]=led[i-2];
-        //change colour when colourlength is reached   
-        if(k>COLORLENGTH)
-        {
-            j++;
-            if(j>7)
-            {
-              j=0;
-            }
-
-            k=0;
-        }
-        k++;
-        //loop colouers
-        
-        //fade red
-        if(led[0].r<(colors[j].r-FADE))
-            led[0].r+=FADE;
-            
-        if(led[0].r>(colors[j].r+FADE))
-            led[0].r-=FADE;
-
-        if(led[0].g<(colors[j].g-FADE))
-            led[0].g+=FADE;
-            
-        if(led[0].g>(colors[j].g+FADE))
-            led[0].g-=FADE;
-
-        if(led[0].b<(colors[j].b-FADE))
-            led[0].b+=FADE;
-            
-        if(led[0].b>(colors[j].b+FADE))
-            led[0].b-=FADE;
-
-		_delay_ms(100);
-		ws2812_setleds(led,MAXPIX);
+        led[0] = colors[7];
+        ws2812_setleds(led,MAXPIX);
+		_delay_ms(1000);
+        led[0] = colors[8];
+        ws2812_setleds(led,MAXPIX);
+        _delay_ms(1000);
     }
 }
